a2b46fc625b1ba2eae0dbab957cdf6979fd0761a
Eric Lachcik <lachcikeric@gmail.com>
2024-03-06 15:35:58 +0100

    Pues sec

--- app/src/test/java/git/flow/AppTest.java
+++ app/src/test/java/git/flow/AppTest.java
@@ -13,6 +13,6 @@ class AppTest {
     }
     @Test void sumaCorrecta(){
         App classUnderTest = new App();
-        assertEquals(7, classUnderTest.sumar(3, 2));
+        assertEquals(8, classUnderTest.sumar(3, 2));
     }
 }
